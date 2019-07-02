3d7a278bd2fbd46f7cd0386bc50c03d632278f4b
陈晓冲 <chenxiaochong@encdata.cn>
2019-07-02 15:21:48 +0800

    增加java文件

---
+++ HelloJava/src/HelloJava.java
@@ -0,0 +1,17 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+        System.out.println("HelloJava!");
+        System.out.println("H2!");
+        fun();
+        System.out.println("H3");
+        System.out.println("H4");
+        System.out.println("H5");
+        
+	}
+    public static void fun() {
+    	System.out.println("HH");
+    }
+}
